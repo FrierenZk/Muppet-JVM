171088437d7a06ff3561d4c9ee14dc58d3ade19b
FrierenZk <lingfengfroze@yahoo.com>
2020-12-03 14:06:42 +0800

    Add a simple real svn repository to perfect test

--- src/test/kotlin/com/github/frierenzk/task/SVNTaskTest.kt
+++ src/test/kotlin/com/github/frierenzk/task/SVNTaskTest.kt
@@ -1,18 +1,37 @@
 package com.github.frierenzk.task
 
-import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import java.io.File
 import java.net.URI
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 internal class SVNTaskTest {
-    private val uri: URI by lazy { File("build").toURI() }
+    private val uri: URI by lazy { File("build/tmp/subversion").toURI() }
 
     @Test
     @Order(1)
-    internal fun infoTest() {
-        val task = SVNTask.buildSVNTask(uri)
-        println(task.info().isBlank())
+    internal fun checkOutTest() {
+        try {
+            File(uri).deleteRecursively()
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            println(exception.message)
+        }
+        val svnPath = "https://svn.apache.org/repos/asf/subversion/trunk/doc/programmer/"
+        assertEquals(SVNTask.isURL(svnPath), true)
+        val task = SVNTask.buildSVNCheckOutTask(uri, svnPath)
+        assertEquals(task.info().isBlank(), false)
+        task.outBufferedReader.forEachLine {
+            println(it)
+        }
+        task.errorBufferedReader.forEachLine {
+            println(it)
+        }
+        task.checkOut()
         task.outBufferedReader.forEachLine {
             println(it)
         }
@@ -23,9 +42,9 @@ internal class SVNTaskTest {
 
     @Test
     @Order(2)
-    internal fun updateTest() {
+    internal fun infoTest() {
         val task = SVNTask.buildSVNTask(uri)
-        task.update()
+        assertEquals(task.info().isBlank(), false)
         task.outBufferedReader.forEachLine {
             println(it)
         }
@@ -36,18 +55,9 @@ internal class SVNTaskTest {
 
     @Test
     @Order(3)
-    internal fun checkOutTest() {
-        val svnPath = "svn://noway"
-        println("path is ${SVNTask.isSVNPath(svnPath)}")
-        val task = SVNTask.buildSVNCheckOutTask(File("build/virtual").toURI(), svnPath)
-        println(task.info().isBlank())
-        task.outBufferedReader.forEachLine {
-            println(it)
-        }
-        task.errorBufferedReader.forEachLine {
-            println(it)
-        }
-        task.checkOut()
+    internal fun updateTest() {
+        val task = SVNTask.buildSVNTask(uri)
+        task.update()
         task.outBufferedReader.forEachLine {
             println(it)
         }
